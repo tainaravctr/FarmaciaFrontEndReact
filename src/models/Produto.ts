13d091068b348b0168ef3279806420f516ec71ce
tainaravctr <tainarisvictoria@gmail.com>
2023-10-17 14:19:59 -0300

    NavBarFooterandHome

---
+++ src/models/Produto.ts
@@ -0,0 +1,12 @@
+import Categoria from './Categoria';
+
+
+export default interface Produto {
+  id: number;
+  nomeProduto: string;
+  descricao: string;
+  precoProduto: number;
+  imgProduto: string;
+
+  categoria: Categoria | null;
+}
